6d7df38efab75a794ca58a1561eed6b82efe3223
Shane Whitehead <obishiro@gmail.com>
2018-09-14 09:11:08 +1000

    initial commit

---
+++ CalendarKit/CalendarKit.h
@@ -0,0 +1,19 @@
+//
+//  CalendarKit.h
+//  CalendarKit
+//
+//  Created by Shane Whitehead on 14/9/18.
+//  Copyright © 2018 KaiZen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalendarKit.
+FOUNDATION_EXPORT double CalendarKitVersionNumber;
+
+//! Project version string for CalendarKit.
+FOUNDATION_EXPORT const unsigned char CalendarKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalendarKit/PublicHeader.h>
+
+
